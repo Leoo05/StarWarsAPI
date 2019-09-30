ba810b81ebcfffa6faab9cd7746016cbaeb79079
Jaime Stiven Leon Tunjano <jaimeleonxd@gmail.com>
2019-09-29 23:29:20 -0500

    Cambio en planets
    Ahora itera entre las paginas del API

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,37 +1,18 @@
 package com.example.myapplication;
 
 import android.content.Intent;
-import android.os.AsyncTask;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.StringWriter;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-
-import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String URL_SWAPI = "https://swapi.co/api/";
     TextView textTitle;
-    String JsonString;
     Button peopleButton;
     Button planetsButton;
-    JSONObject obj;
 
 
     @Override
@@ -43,62 +24,14 @@ public class MainActivity extends AppCompatActivity {
         planetsButton = findViewById(R.id.button_planetas);
     }
 
-    public void makeCallPeople(View v) throws JSONException {
-        /*callWebService("people/");
-        if (obj != null) {
-            Intent next = new Intent(MainActivity.this, People.class);
-            next.putExtra("json", obj.getString("results"));
-            next.putExtra("nextPage", obj.getString("next"));
-            startActivity(next);
-        }*/
+    public void makeCallPeople(View v){
         Intent next = new Intent(MainActivity.this, People.class);
         startActivity(next);
     }
 
-    public void makeCallPlanets(View v) throws JSONException {
-        if(obj !=null) {
+    public void makeCallPlanets(View v) {
             Intent next = new Intent(MainActivity.this, Planets.class);
             startActivity(next);
-        }
+
     }
-
- /*   public void callWebService(final String serviceEndPoint) {
-        AsyncTask.execute(new Runnable() {
-            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-            @Override
-            public void run() {
-                try {
-                    URL urlService = new URL(URL_SWAPI + serviceEndPoint);
-                    HttpsURLConnection connection = (HttpsURLConnection) urlService.openConnection();
-                    connection.setRequestMethod("GET");
-                    if (connection.getResponseCode() == 200) {
-                        InputStream responseBody = connection.getInputStream();
-                        // Success
-                        JsonString = getStringFromInputStream(responseBody);
-                        obj = new JSONObject(JsonString);
-                    } else {
-                        // Error handling code goes here
-                        Log.v("ERROR", "ERROR");
-                    }
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
-
-
-    public static String getStringFromInputStream(InputStream stream) throws IOException {
-        int n = 0;
-        char[] buffer = new char[1024 * 4];
-        InputStreamReader reader = new InputStreamReader(stream, "UTF-8");
-        StringWriter writer = new StringWriter();
-        while (-1 != (n = reader.read(buffer))) writer.write(buffer, 0, n);
-        return writer.toString();
-    }*/
 }
--- app/src/main/java/com/example/myapplication/Planets.java
+++ app/src/main/java/com/example/myapplication/Planets.java
@@ -1,9 +1,11 @@
 package com.example.myapplication;
 
+import android.os.AsyncTask;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,17 +14,26 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class Planets extends AppCompatActivity {
 
-    String json;
     ArrayList<Planeta> listaPlanetas;
     Button sig;
     Button ant;
+    public final String URL_SWAPI = "https://swapi.co/api/planets/";
     TextView atr;
     int index=0;
-    String nextPage;
+    String JsonString;
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
@@ -35,11 +46,7 @@ public class Planets extends AppCompatActivity {
         atr = findViewById(R.id.textView_atributos_planetas);
 
         this.listaPlanetas = new ArrayList<>();
-        json = getIntent().getStringExtra("json");
-        nextPage = getIntent().getStringExtra("nextPage");
-        this.agregarPlanetas();
-        atr.setText(listaPlanetas.get(index).toString());
-
+        callWebService(URL_SWAPI);
 
         sig.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,9 +81,63 @@ public class Planets extends AppCompatActivity {
 
     }
 
-    public void agregarPlanetas() {
+    public void callWebService(final String serviceEndPoint) {
+        AsyncTask.execute(new Runnable() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void run() {
+                try {
+                    URL urlService = new URL(serviceEndPoint);
+                    HttpsURLConnection connection = (HttpsURLConnection) urlService.openConnection();
+                    connection.setRequestMethod("GET");
+                    if (connection.getResponseCode() == 200) {
+                        // Success
+                        InputStream responseBody = connection.getInputStream();
+                        JsonString = getStringFromInputStream(responseBody);
+                        JSONObject obj = new JSONObject(JsonString);
+                        int x = 0;
+                        while(!obj.getString("next").equals("null") && x < 11){
+                            agregarPlanetas(obj.getString("results"));
+                            urlService = new URL(obj.getString("next"));
+                            connection = (HttpsURLConnection) urlService.openConnection();
+                            connection.setRequestMethod("GET");
+                            if (connection.getResponseCode() == 200) {
+                                // Success
+                                responseBody = connection.getInputStream();
+                                JsonString = getStringFromInputStream(responseBody);
+                                obj = new JSONObject(JsonString);
+                            } else {
+                                // Error handling
+                                Log.v("ERROR", "ERROR");
+                            }
+                        }
+                    } else {
+                        // Error handling
+                        Log.v("ERROR", "ERROR");
+                    }
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public static String getStringFromInputStream(InputStream stream) throws IOException {
+        BufferedReader r = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
+        StringBuilder total = new StringBuilder();
+        for (String line; (line = r.readLine()) != null; ) {
+            total.append(line).append('\n');
+        }
+        return total.toString();
+    }
+
+    public void agregarPlanetas(String s) {
         try {
-            JSONArray jsonArray = new JSONArray(json);
+            JSONArray jsonArray = new JSONArray(s);
             for (int i = 0; i < jsonArray.length(); i++) {
                 JSONObject planeta = jsonArray.getJSONObject(i);
                 this.listaPlanetas.add(new Planeta(planeta.getString("name"), planeta.getInt("rotation_period"),
